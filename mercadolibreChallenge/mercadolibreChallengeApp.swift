51a63fe0279b9b6988c00855413de3f6749669c7
joulsCarvajal <juliancarvajaldiaz1992@outlook.com>
2024-07-03 10:42:45 -0500

    Initial Commit

---
+++ mercadolibreChallenge/mercadolibreChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mercadolibreChallengeApp.swift
+//  mercadolibreChallenge
+//
+//  Created by Julian Carvajal on 3/07/24.
+//
+
+import SwiftUI
+
+@main
+struct mercadolibreChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
